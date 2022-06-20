d35739f5e136dae4adb0f165ab884ad561af6e5a
Victor Corassa <victorcorassa@gmail.com>
2022-06-20 10:06:52 -0300

    first commit

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: [ './src' ],
+          extensions: ['.ts', '.tsx', '.js' , '.json'],
+          alias: {
+            '@components': './src/components',
+            '@constants': './src/constants',
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+          }
+        }
+      ]
+    ]
   };
 };
